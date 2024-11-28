4707aa39239bec5d2a1671d3cd1c58e8a52500d2
Vinicius <39202981+Vinicius-Brito-Costa@users.noreply.github.com>
2024-11-28 00:00:00 +0000

    Search for the folder with the plugin.cfg

--- repository/repository.go
+++ repository/repository.go
@@ -21,16 +21,16 @@ type Repository interface {
 	Download(name string, version string, destiny string) bool
 }
 
-type Github struct {}
+type Github struct{}
 
-func (g Github) Download(name string, version string, destiny string) (bool) {
+func (g Github) Download(name string, version string, destiny string) bool {
 	if len(name) == 0 || len(version) == 0 {
 		util.Info("Cannot download. Name or Version missing. Name: " + name + " Version: " + version)
 		return false
 	}
 	var url = "https://github.com/" + name + "/archive/refs/tags/" + version + ".zip"
 
-	response, err := http.Get(url)
+	var response, err = http.Get(url)
 
 	if err != nil {
 		util.Error(err.Error(), err)
@@ -39,40 +39,61 @@ func (g Github) Download(name string, version string, destiny string) (bool) {
 	defer response.Body.Close()
 
 	if response.StatusCode != 200 {
-		util.Info("GET request on " + url + " failed.")
-		return false
+		//https://github.com/Burloe/GoLogger/releases/download/1.2/GoLogger-1.2.zip
+		var splitedName = strings.Split(name, "/")
+		url = "https://github.com/" + name + "/releases/download/" + version + "/" + splitedName[len(splitedName)-1] + "-" + version + ".zip"
+		response, err = http.Get(url)
+		if err != nil {
+			util.Error(err.Error(), err)
+			return false
+		}
+		if response.StatusCode != 200 {
+			util.Info("GET request on " + url + " failed.")
+			return false
+		}
 	}
 
 	body, err := io.ReadAll(response.Body)
-    if err != nil {
-        util.Error(err.Error(), err)
+	if err != nil {
+		util.Error(err.Error(), err)
 		return false
-    }
+	}
 
-    zipReader, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
-    if err != nil {
-        util.Error(err.Error(), err)
+	zipReader, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
+	if err != nil {
+		util.Error(err.Error(), err)
 		return false
-    }
+	}
 
 	var folderWithFiles string = ""
-    // Read all the files from zip archive
-    for _, zipFile := range zipReader.File {
-		if !zipFile.FileInfo().IsDir() && (len(folderWithFiles) == 0 || folderWithFiles > filepath.Dir(zipFile.Name)) {
-			folderWithFiles = filepath.Dir(zipFile.Name)
+	// Read all the files from zip archive
+	for _, zipFile := range zipReader.File {
+
+		// Search for the folder that contains the plugin.cfg file
+		if strings.Contains(zipFile.Name, "plugin.cfg") {
+			if len(folderWithFiles) == 0 {
+				folderWithFiles = filepath.Dir(zipFile.Name)
+			}
+			// If there's another plugin.cfg file in the project
+			// we need to get the upper most folder of the project
+			// so we compare the size of the path between them
+			// the lowest one wins.
+			if len(folderWithFiles) > len(filepath.Dir(zipFile.Name)) {
+				folderWithFiles = filepath.Dir(zipFile.Name)
+			}
 		}
 
-        err := extract(zipFile, destiny)
-        if err != nil {
-            util.Error(err.Error(), err)
+		err := extract(zipFile, destiny)
+		if err != nil {
+			util.Error(err.Error(), err)
 			continue
-        }
-    }
+		}
+	}
 
 	util.Trace("Folder with files: " + folderWithFiles)
-	
+
 	var split = strings.Split(folderWithFiles, string(os.PathSeparator))
-	copyUtil.Dir(destiny + string(os.PathSeparator) + folderWithFiles, destiny + string(os.PathSeparator) + split[len(split) - 1])
+	copyUtil.Dir(destiny+string(os.PathSeparator)+folderWithFiles, destiny+string(os.PathSeparator)+split[len(split)-1])
 	os.RemoveAll(destiny + string(os.PathSeparator) + split[0])
 	return true
 }
@@ -87,10 +108,10 @@ func extract(f *zip.File, dest string) error {
 	path := filepath.Join(dest, f.Name)
 
 	// Check for ZipSlip (Directory traversal)
-	if !strings.HasPrefix(path, filepath.Clean(dest) + string(os.PathSeparator)) {
+	if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
 		return fmt.Errorf("Illegal file path: %s", path)
 	}
-	
+
 	if f.FileInfo().IsDir() {
 		os.MkdirAll(path, f.Mode())
 	} else {
